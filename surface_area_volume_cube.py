c73ae11d912f0ebdc910e4861fad856ee3c0179d
Noah Smith <noah.smith@stu.ocsb.ca>
2023-10-29 21:38:17 +0000

    fixed if else statement

--- surface_area_volume_cube.py
+++ surface_area_volume_cube.py
@@ -15,18 +15,18 @@ def main():
     # Check if the side length is greater than 0
     if side_length <= 0:
         print("Please enter a number greater than 0!")
+    else:
+        # Get the unit of measurement
+        unit = input("Enter the unit of measurement: ")
 
-    # Get the unit of measurement
-    unit = input("Enter the unit of measurement: ")
+        # Calculate the volume and surface area
+        volume = side_length * side_length * side_length
+        surface_area = side_length * side_length * 6
 
-    # Calculate the volume and surface area
-    volume = side_length * side_length * side_length
-    surface_area = side_length * side_length * 6
-
-    # Display the volume and the surface area back to the user, with proper units
-    print("")
-    print("The volume is {:.2f}{}^3".format(volume, unit))
-    print("The surface_area is {:.2f}{}^2".format(surface_area, unit))
+        # Display the volume and the surface area back to the user, with proper units
+        print("")
+        print("The volume is {:.2f}{}^3".format(volume, unit))
+        print("The surface_area is {:.2f}{}^2".format(surface_area, unit))
 
 
 if __name__ == "__main__":
